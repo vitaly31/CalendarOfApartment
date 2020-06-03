82a7db089dc968f5c6baa2ebe1506f50535b95d0
Vitalii <vitos77794@gmail.com>
2020-06-03 18:46:41 +0300

    Initial Commit

---
+++ CalendarForApartments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalendarForApartments
+//
+//  Created by Виталий Косинов on 03/06/2020.
+//  Copyright © 2020 Виталий Косинов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
